125a6e6eb285429457d2e375def1a4d9e37f0a4a
DawidCwiek <cwiekdawid1@gmail.com>
2020-06-29 13:39:37 +0200

    add Book model

---
+++ Library/Library/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [StringLength(250, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [StringLength(250, MinimumLength = 3)]
+        public string Author { get; set; }
+
+        [StringLength(250, MinimumLength = 3)]
+        public string PublishingHouse { get; set; }
+
+        [Display(Name = "Release Date"), DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+    }
+}
