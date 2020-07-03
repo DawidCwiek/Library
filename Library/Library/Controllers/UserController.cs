51ec0476a22b53c86ccc24878dcbcc8a823693d1
urszulasologub <urszula.sologub@gmail.com>
2020-07-03 18:01:19 +0200

    Added controller returning user's borrowings

---
+++ Library/Library/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Library.Areas.Identity.Data;
+using Library.Data;
+using Library.Migrations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly LibraryContext _context;
+
+        public UserController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: UserController
+        public ActionResult Index()
+        {
+            var users_borrowing = _context.Borrowing.Include(b => b.ApplicationUser).Include(b => b.Book).Where(b => b.ApplicationUser.UserName == User.Identity.Name);
+            return View(users_borrowing);
+        }
+
+        
+    }
+}
